24454c8be29d2a120cfa39316d8788deb5b45eca
vageeshn <alokatulya@gmail.com>
2021-02-02 18:26:26 +0530

    test

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
-
+var gameState="onSling";
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -69,16 +69,20 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});  
+    }
+    
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState="launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+        //slingshot.attach(bird.body);
     }
 }
